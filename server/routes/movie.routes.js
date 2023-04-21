9d093ac0b72795e2276ca614cd5323be9b9eb85b
Matthew York <myorkprogramming@gmail.com>
2023-04-20 19:20:26 -0700

    YORK-- small update

--- server/routes/movie.routes.js
+++ server/routes/movie.routes.js
@@ -2,9 +2,19 @@ const MovieController = require('../controller/movie.controller');
 
 
 // 	module.exports = (app) => {
+
+/* Movies */
 // 	app.get('/api/movies', MovieController.findAllMovies);
 // 	app.post('/api/movies', MovieController.createNewMovie);
 // 	app.get('/api/movies/:id', MovieController.findOneMovie);
 // 	app.delete('/api/movies/:id', MovieController.deleteOneMovie);
 // 	app.put('/api/movies/:id', MovieController.updateMovie);
+    
+/* Users */
+// app.get("/:id", getUser);                  
+// app.get("/edit/:id", getUser);                 
+// app.get("/:id/friends", getUserFriends);
+// app.patch("/:id/:friendId", addRemoveFriend);
+// app.put("/:id", updateUserProfile);
+
 // }
