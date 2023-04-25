13599e492b371df96fd7452a5963b93773ec6f8f
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2023-04-24 21:14:32 -0400

    Update Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,11 +5,86 @@ import '../App.css';
 
 function Dashboard(props) {
     const navigate = useNavigate();
+    const [title, setTitle] = useState("");
+    const [genre, setGenre] = useState("");
+    const [rating, setRating] = useState("");
+    const [review, setReview] = useState("");
+    
+    const [errors, setErrors] = useState({});
 
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8000/api/movies/new", {
+                title,
+                genre,
+                review,
+                rating
+            })
+            .then((res) => {
+                console.log(res.data)
+                navigate('/')
+                setTitle("")
+                setGenre("")
+                setReview("")
+                setRating("")
+
+            })
+            .catch((err) => {
+                console.log(err)
+                console.log("Validation Error from the back end")
+                setErrors(err.response.data.errors)
+            })
+    }
 
     return (
     <   div className="Dashboard">
-            Hey yall
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <div>
+                        <label style={{display: 'inline-block'}}>Title</label><br />
+                        <input style= {{}}
+                            type="text" 
+                            onChange={(e) => setTitle(e.target.value)}
+                            //value={title}
+                            />
+                        {errors.title ? <span style={{margin: '10px',color:"red", fontWeight: 'normal'}}>{errors.make.message}</span> : null}
+                    </div>
+
+                    <div>
+                        <label style={{display: 'inline-block', marginRight: '10px'}}>Genre</label><br />
+                        <input style= {{}}
+                            type="text" 
+                            onChange={(e) => setGenre(e.target.value)}
+                            //value={genre}
+                            />
+                        {errors.genre ? <span style={{margin: '10px',color:"red", fontWeight: 'normal'}}>{errors.modelName.message}</span> : null}
+                    </div>
+
+                    <div>
+                        <label>Review</label><br />
+                        <input
+                            type="text" 
+                            onChange={(e) => setReview(e.target.value)}
+                            //value={review}
+                            />
+                        {errors.review ? <span style={{margin: '10px',color:"red", fontWeight: 'normal'}}>{errors.year.message}</span> : null}
+                    </div>
+
+                    <div>
+                        <label>Rating</label><br />
+                        <input
+                            type="text" 
+                            onChange={(e) => setRating(e.target.value)}
+                            //value={rating}
+                            />
+                        {errors.rating ? <span style={{margin: '10px',color:"red", fontWeight: 'normal'}}>{errors.price.message}</span> : null}
+                    </div>
+
+                    <button style={{cursor: 'pointer', width: '50px', height: '20px', textAlign: 'center'}}>Submit</button>
+
+                </div>
+            </form>
 
         </div>
     );
