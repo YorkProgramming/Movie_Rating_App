404ee6a176e7e8716e80f7c6e04225532615f32a
Seth Campbell <campbell.setha@gmail.com>
2023-04-25 17:04:00 -0700

    Add files via upload
    Adding the views folder

---
+++ client/src/views/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Content from '../components/Content';
+import Header from '../components/Header'
+
+const Dashboard = (props) => {
+
+  return (
+    <>
+      <Header/>
+      <Content/>
+    </>
+  )
+}
+export default Dashboard;
