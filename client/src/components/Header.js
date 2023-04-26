abe488e747cc19c5f45392e7e2b6ef06f1b59e90
mrshawnmckee <116242881+mrshawnmckee@users.noreply.github.com>
2023-04-25 21:02:40 -0400

    Update Header.js
    Again, navbar. Yall dont have to keep it there, mostly threw it there to make getting around easier

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,11 @@ function Header (props) {
 
   return (
     <>
+      <div>
+        <a href='/' style={{margin:"10px 30px 10px 30px", textDecoration:'none', color: "black"}}>Login</a>
+        <a href='/dashboard' style={{margin:"10px 30px 10px 30px", textDecoration:'none', color: "black"}}>Rate a Movie</a>
+        <a href='/movies' style={{margin:"10px 30px 10px 30px", textDecoration:'none', color: "black"}}>Browse</a>
+      </div>
         <h1>Movie Rating Database</h1>
     </>
   );
